200000b5f89cc8773759108f359123c04991b7c7
Maryna <textconstruction@protonmail.com>
2021-12-20 20:09:39 +0100

    Integrate EF Core to the project

---
+++ FreakyFashionServices.CatalogService/Migrations/20211220190544_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreakyFashionServices.CatalogService.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ArticleNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlSlug = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ FreakyFashionServices.CatalogService/Models/Domain/Product.cs
@@ -0,0 +1,43 @@
+﻿namespace FreakyFashionServices.CatalogService.Models.Domain
+{
+    public class Product
+    {
+        public Product(int id, 
+                       string name, 
+                       string description, 
+                       string imageUrl, 
+                       double price, 
+                       string articleNumber)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+            ArticleNumber = articleNumber;
+            UrlSlug = name.Trim().Replace("-", "").Replace(" ", "-").ToLower(); 
+        }
+
+        public Product(string name,
+                       string description,
+                       string imageUrl,
+                       double price,
+                       string articleNumber)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+            ArticleNumber = articleNumber;
+            UrlSlug = name.Trim().Replace("-", "").Replace(" ", "-").ToLower();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public string ArticleNumber { get; set; }
+        public string UrlSlug { get; set; }
+    }
+}
